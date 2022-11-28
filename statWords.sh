f3e7b04552f003e2718581af72f163856258b95b
Facundo344 <73924327+Facundo344@users.noreply.github.com>
2022-11-28 14:47:18 +0000

    simplificar a una sola función

--- statWords.sh
+++ statWords.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-function longestWord {
+function word_stats {
 sed 's/\b /\n/g' Text.txt | sort | uniq | awk '{print length, $0}' | sort -nr | PALABRA_ML= head -n 1
-echo "La palabra más larga es $PALABRA_ML"
-}
 
-function shortestWord {
 sed 's/\b /\n/g' Text.txt | sort | uniq | awk '{print length, $0}' | sort -nr | PALABRA_MC = tail -n 1
-echo "La palabra más corta es $PALABRA_MC"
-}
 
-function averageLength {
 NRO_PALABRAS= wc -w Text.txt
 NRO_LETRAS= wc -c sed 's/[^a-zA-Z]//g' Text.txt
 PROMEDIO_LONGITUD= [$NRO_PALABRAS / $NRO_LETRAS]
-echo "El promedio de longitud por palabra es de $PROMEDIO_LONGITUD letras"
+
+
+
+echo "La palabra más larga es $PALABRA_ML
+La palabra más corta es $PALABRA_MC
+El promedio de longitud por palabra es de $PROMEDIO_LONGITUD letras"
 }
