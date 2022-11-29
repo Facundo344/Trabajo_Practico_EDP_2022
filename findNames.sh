cf968a04b21bab047d524044d975421d2405e11d
Facundo344 <73924327+Facundo344@users.noreply.github.com>
2022-11-29 16:58:37 +0000

    Último acá

--- findNames.sh
+++ findNames.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function find_names {
-sed -e 's/[[:punct:]]*//g;s/[[:space:]]\+/\n/g' Text.txt | grep -P '\b[[:upper:]]' >> names
+sed -e 's/[[:punct:]]*//g;s/[[:space:]]\+/\n/g' Text.txt | grep -P '\b[[:upper:]]' > names
 cat names 
 }
 
