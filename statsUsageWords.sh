a0a29e8f390177e11b7fe2165f36e5467ef322fd
Facundo344 <73924327+Facundo344@users.noreply.github.com>
2022-11-29 16:47:47 +0000

    Último commit

--- statsUsageWords.sh
+++ statsUsageWords.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 function word_usage {
-sed -e 's/[[:punct:]]*//g;s/[[:space:]]\+/\n/g' Text.txt | egrep -o '[^ ]{4,}' | sort | uniq -c | sort -bnr | Top_10 = head
-echo "$Top_10"
+sed -e 's/[[:punct:]]*//g;s/[[:space:]]\+/\n/g' Text.txt | egrep -o '[^ ]{4,}' | sort | uniq -c | sort -bnr | head > Top_10
+cat Top_10
 }
+
+word_usage
