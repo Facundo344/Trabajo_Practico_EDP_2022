54fb7fd8415594314be11500e3be9e9d1006e657
Facundo344 <73924327+Facundo344@users.noreply.github.com>
2022-11-30 17:36:30 +0000

    D

--- statWords.sh
+++ statWords.sh
@@ -4,7 +4,7 @@ function word_stats {
 PALABRA_ML=$(sed -e 's/[[:punct:]]*//g;s/[[:space:]]\+/\n/g' Text.txt | sort | uniq | awk '{print length, $0}' | sort -nr | awk '{print $2}' | head -n 1)
 PALABRA_MC=$(sed -e 's/[[:punct:]]*//g;s/[[:space:]]\+/\n/g' Text.txt | sort | uniq | sed '/^$/d' | awk '{print length, $0}' | sort -n | awk '{print $2}' | head -n 1)
 
-NRO_PALABRAS=$(wc -w | awk '{print $1}')
+NRO_PALABRAS=$(wc -w Text.txt | awk '{print $1}')
 NRO_LETRAS=$(sed -e 's/[[:punct:]]*//g' Text.txt | wc -c)
 
 PROMEDIO_LONGITUD=$(($NRO_LETRAS/$NRO_PALABRAS))
