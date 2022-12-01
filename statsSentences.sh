492d4ee8d405f568396bc184d2915372c6a28c12
Facundo Bartolotta <feriaairef@gmail.com>
2022-12-01 17:26:12 -0300

    Un pequeño error

--- statsSentences.sh
+++ statsSentences.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 function sentence_stats {
-sed -e 's/\n//g' Text.txt | sed 's/[.!?]  */&\n/g' > oraciones
+sed 's/[.!?]  */&\n/g' Text.txt | sed '/^$/d' > oraciones
 
-ORACION_ML=$(sed -e 's/[[:punct:]]*//g;s/[[:space:]]\+/\n/g' oraciones | awk '{print length, $0}' | sort -nr | awk '{print $2}' | head -n 1)
-ORACION_MC=$(sed -e 's/[[:punct:]]*//g;s/[[:space:]]\+/\n/g' oraciones | sed '/^$/d' | awk '{print length, $0}' | sort -n | awk '{print $2}' | head -n 1)
+ORACION_ML=$(awk '{print length, $0}' oraciones | sort -nr | awk '{$1=""; print}' | head -n 1)
+ORACION_MC=$(awk '{print length, $0}' oraciones | sort -n | awk '{$1=""; print}' | head -n 1)
 
 NRO_ORACIONES=$(sed -n '$=' oraciones)
 NRO_LETRAS=$(sed -e 's/[[:punct:]]*//g' Text.txt | wc -c)
@@ -12,8 +12,8 @@ NRO_LETRAS=$(sed -e 's/[[:punct:]]*//g' Text.txt | wc -c)
 PROMEDIO_LONGITUD=$(($NRO_LETRAS/$NRO_ORACIONES))
 
 
-echo "La oración más larga es $ORACION_ML
-La oración más corta es $ORACION_MC
+echo "La oración más larga es: $ORACION_ML
+La oración más corta es: $ORACION_MC
 El promedio de longitud por oración es de $PROMEDIO_LONGITUD letras"
 }
 
